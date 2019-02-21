be54f9655f78241b9348c71c5a91107e10af66fd
Bob Rockers <rockers@okstate.edu>
2019-02-20 21:20:58 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// CIS2013_Week05_Lab1
+
+const int MAX = 30;
+// List of student in our class
+int student[MAX];
+string name[MAX];
+
+void makeStudent(int x){
+	cout << "Enter Student " << x+1 << "'s name ";
+	cin >> name[x];
+	cout << "  Enter " << name[x] << "'s grade: ";
+	cin >> student[x];
+}
+
+void printGrade(int y){
+	cout << name[y] << "'s grade is " << student[y] << endl;
+}
+
+int main(){
+
+	int LEN;
+	
+	cout << "How many students are in this class: ";
+	cin >> LEN;
+	
+	for(int i=0; i<LEN; i++){
+		makeStudent(i);
+	}
+	
+	for(int j=0; j<LEN; j++){
+		printGrade(j);
+	}
+	
+	return 0;
+}
